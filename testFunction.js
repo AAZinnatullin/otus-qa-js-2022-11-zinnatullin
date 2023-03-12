de3602abf78c97d4a4ff2c717f9c381eda4da57a
Artyom Zinnatullin <82364379+AAZinnatullin@users.noreply.github.com>
2023-03-12 15:56:34 +0400

    Added tests with playwright for project work (#5)
    Co-authored-by: Artyom <artyom.zn@plyo.io>

--- testFunction.js
+++
@@ -1,7 +0,0 @@
-import { strict as assert } from 'node:assert';
-
-function sum(a, b) {
-    return a + b;
-}
-
-assert.equal(sum(2, 2), 4);
---
+++ tests/basicAuth.spec.js
@@ -0,0 +1,14 @@
+// @ts-check
+const { test } = require('@playwright/test');
+const { pageData, locators } = require('../fixtures/commonData');
+
+test.use({ extraHTTPHeaders: {
+    Authorization: 'Basic dXNlcjpwYXNz'
+    } });
+
+test('basic auth with auth header', async ({ page }) => {
+    await page.goto('/')
+    await page.getByText('HTTP/NTLM Auth', { exact: true }).click();
+
+    await pageData.isLogin(page, locators.login.successUrl, locators.login.successText);
+})
